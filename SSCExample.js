7ebe2189255fe9c8a8f16346abe3645c1f78a3e9
Nick Cesare <ncesare@agoragames.com>
2016-12-12 13:09:25 -0500

    Fixed startsWith.

--- SSCExample.js
+++ SSCExample.js
@@ -719,7 +719,7 @@ if (!String.prototype.startsWith) {
 
 Hydra.get('custom_starts_with_test', function(requst, response){
     var theString = "test123";
-    if(theString.startsWith("bro")){
+    if(theString.startsWith("test")){
         Logger.info("Yes, string starts with test.");
     }else{
         Logger.info("No, string starts with something else.");
