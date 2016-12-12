8adff3150f8d25671c902f91086f88f9caa478b6
Nick Cesare <ncesare@agoragames.com>
2016-12-12 12:49:14 -0500

    Capitalization

--- SSCExample.js
+++ SSCExample.js
@@ -710,7 +710,7 @@ Hydra.get('custom_no_response', function(request, response) {});
 //Custom 'StartsWith' Test
 Hydra.get('custom_starts_with_test', function(requst, response){
     var theString = "test123";
-    if(theString.StartsWith("test")){
+    if(theString.startsWith("test")){
         Logger.info("Yes, string starts with test.");
     }
     return {};
