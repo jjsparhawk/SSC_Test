ec4d1a8f6acc18711e68d202140b3e79c45c7738
Nick Cesare <ncesare@agoragames.com>
2017-01-06 13:44:26 -0500

    Custom Invalidate Example

--- SSCExample.js
+++ SSCExample.js
@@ -739,6 +739,10 @@ Hydra.get('custom_global_get_all', function(request, response) {
     response.success({"ret": Global.getAll()})
 });
 
+//Custom Endpoint to Invalidate SSC
+Hydra.get('custom_invalidate', function(request, response) {
+    client.invalidate();
+});
 //------------------------------------------------------------------------------------------------------------------------------------------
 
 
