b36f26657e4237ff674d9556bfe19474152e7225
Nick Cesare <ncesare@agoragames.com>
2017-04-25 17:25:36 -0400

    different syntax

--- SSCExample.js
+++ SSCExample.js
@@ -769,12 +769,13 @@ Hydra.get('custom_get_without_headers', function(request, response) {
 });
 
 //Custom endpoint to test Profile Response Body
-Hydra.put('custom_test_profile_response_body', function(request, response) {
+Hydra.put('custom_test_profile_response_body', function(request) {
     var serverAuth = Hydra.Client.authServer();
     var profileToRetrieve = "/profiles/" + request.body['account_id'];
-    Hydra.Client.get(profileToRetrieve, {"auth":serverAuth}, function(profileResponse, body) {
-        response.success(profileResponse.response.body);
-    });
+    return Hydra.Client.get(profileToRetrieve, {"auth":serverAuth})
+        .then(function (profile) {
+            return D.resolved(profile.response.body)
+        });
 });
 
 Hydra.get('custom_get_without_headers', function(request, response) {
