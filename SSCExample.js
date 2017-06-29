89a2fc3fdb2ff861d4ebc543138bb7a62cdb7052
Tyler Trulson <tjtrulson@Tylers-MacBook-Pro.local>
2017-06-29 14:28:23 -0400

    Re-adding response.success

--- SSCExample.js
+++ SSCExample.js
@@ -26,7 +26,7 @@ Hydra.onLoad(function(response) {
 Hydra.account.beforeCreate(function(request, response){
     Logger.info("Before Account Create Log");
     // Pass back that the hook was hit regardless of if the account was created or not
-    return({"Before Account Create Hit": true});
+    response.success({"Before Account Create Hit": true});
     
 })
 
@@ -262,7 +262,7 @@ Hydra.match.afterFixedCreate(function(request, response){
 
 Hydra.object.beforeCreate(function(request, response){
     Logger.info("Before Generic Object Create Log");
-    response.eventEmit({"Before Object Create Hit": true})
+    response.success({"Before Object Create Hit": true})
     //return([['set', 'response.body', {"Before Object Create Hit": true}]]);
 })
 
