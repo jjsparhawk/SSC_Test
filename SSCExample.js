5e72885c6eec00e0de646b7c66e7c335ff88960e
Tyler Trulson <tjtrulson@Tylers-MacBook-Pro.local>
2017-08-24 16:47:43 -0400

    Trying different syntax

--- SSCExample.js
+++ SSCExample.js
@@ -319,7 +319,7 @@ Hydra.object.beforeUpdate(function(request, response){
 
 Hydra.object.afterUpdate(function(request, response){
     Logger.info("After Generic Object Update Log");
-    Logger.info(response["data"]);
+    Logger.info(response["body"]["data"]);
     return {};
 })
 
