e9a8d824228b81c3a8123ba3dc90d97a85b590f7
Tyler Trulson <tjtrulson@Tylers-MacBook-Pro.local>
2017-09-12 13:37:56 -0400

    Testing something else

--- SSCExample.js
+++ SSCExample.js
@@ -1584,17 +1584,12 @@ Hydra.put('decompress_profile_field', function(request, response){
 
     var profileToRetrieve = "/profiles/" + request.body['account_id'];
 
-    Hydra.Client.get(profileToRetrieve, {"auth": serverAuth}, function(profileResponse, body) {
-        if(profileResponse.statusCode == 200){
-            var theCompressedData = body["data"]['field_to_compress'];
-            Logger.info(body["data"]["field_to_compress"])
-            var decompressed = theCompressedData.decompressSync();
-
-            Logger.info("Decompressed Data: " +  JSON.stringify(decompressed));
-            response.success({"Our Data: ": JSON.stringify(decompressed)});
-        } else {
-            response.failure({})
-        }
+    Hydra.Client.get(profileToRetrieve, {auth: serverAuth})
+    .then(function(result) {
+        var theCompressedData = result.body["data"][request.body['field_to_compress']];
+        var decompressed = theCompressedData.decompressSync();
+        Logger.info("Decompressed Data: " +  JSON.stringify(decompressed));
+        response.success({"Our Data: ": JSON.stringify(decompressed)});
     })
 });
 
