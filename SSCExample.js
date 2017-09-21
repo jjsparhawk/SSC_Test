30e7dac897e25c6fb89b4a8763deea6ce6b6c83a
Will Macaluso <will.macaluso@wbgames.com>
2017-09-21 12:17:28 -0400

    g

--- SSCExample.js
+++ SSCExample.js
@@ -77,7 +77,7 @@ Hydra.account.afterKick(function(request, response){
 
 Hydra.account.beforeAuth(function(request, response){
             var serverAuth = Hydra.Client.authServer();
-            return Hydra.Client.get("/broadcast_channels/test/BeforeMatchLeaveWasJustHit", {auth: serverAuth, will: request.model['account_id']});
+            return Hydra.Client.get("/broadcast_channels/test/BeforeMatchLeaveWasJustHit2", {auth: serverAuth, will: request.model['account_id']});
     Logger.info("Before Account Auth Log");
     return {};
 
