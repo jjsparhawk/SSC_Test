2cf3dfd0ab0172fd964c4ce54d633c84398a9dc2
elendiastarman <elendiastarman@users.noreply.github.com>
2017-10-02 14:14:37 -0400

    Update SSCExample.js

--- SSCExample.js
+++ SSCExample.js
@@ -1036,13 +1036,15 @@ Hydra.get('return_large_number', function(request, response) {
 
 Hydra.put('emit_external_event', function(request, response) {
     Event.emit("{'kind': 'CAT', 'name': 'pongo'}", {'id': 'test_pet_schema'}).then(function() {
-        response.success({"ret": "here"});
+        return {"ret": "here"};
     });
 });
 
 Hydra.put('emit_user_external_event', function(request, response) {
     Event.emit({'kind': 'CAT', 'name': 'pongo', 'user_id': '1234-5678-9101112'}, {'id': 'test_pet_owner_schema'}).then(function() {
-        response.success({"ret": "this"});
+        return {"ret": "this"};
+    }, function() {
+        return {"ret": "error"};
     });
 });
 
