ee95a17b13d085a16e0c9db9e4069eecabc7d845
Robert J Stewart <robert.stewart@wbgames.com>
2018-11-01 15:04:23 -0400

    attempt at beforeupdate syntax fixes

--- profile.js
+++ profile.js
@@ -2,15 +2,18 @@
 Hydra.profile.beforeUpdate(function(request){
 	let banlist = Global.get("restricted");
 	let acceptable = true;
-	for (var bannedWord in banlist) {
-		if (request.userRequest.body.data.myNickname === bannedWord){
-			acceptable = false;
+	if (request.userRequest.body.Operations[2] === 'myNickname') {
+		for (var bannedWord in banlist) {
+			if (request.userRequest.body.Operations[3] === bannedWord){
+				acceptable = false;
+			}
 		}
-	}
-	if (acceptable){
-		return{};
-	}
-	else{
-		return D.rejected({'msg' : 'Innappropriate Nickname Submitted'});
+		if (acceptable){
+			return {};
+		} else{
+			return D.rejected({'msg' : 'Innappropriate Nickname Submitted'});
+		}
+	} else {
+		return {};
 	}
 })
