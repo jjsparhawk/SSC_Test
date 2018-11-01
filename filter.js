4e15bb56ab80390562237bfd7ea5f7c7f128e815
Robert J Stewart <robert.stewart@wbgames.com>
2018-11-01 17:11:10 -0400

    breaking out profanity filter

--- filter.js
+++ filter.js
@@ -1,6 +1,7 @@
 // Innappropriate words filter
 function validateWord(word){
 	word = word.replace(/\s+/g, '');
+	word = word.toLowercase();
 	for (var bannedWord of Global.get("restricted")) {
 		if (word.includes(bannedWord)){
 			return false;
