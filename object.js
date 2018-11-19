bfa7efee20bcebe021d568300239345a8cbfa54e
Robert J Stewart <robert.stewart@wbgames.com>
2018-11-19 15:21:13 -0500

    syntax fixes

--- object.js
+++ object.js
@@ -2,5 +2,5 @@ Logger.level = Logger.INFO;
 //custom endpoint and afterHook to test new header
 Hydra.object.afterCreate(function(request){
 	Logger.info("After Profile Update Log");
-	return ['inc', 'server_data.timesBeforeProfileUpdateHit', 1];
+	return [['inc', 'server_data.timesAfterProfileUpdateHit', 1]];
 })
