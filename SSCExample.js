96ef566876ff65bee8c662df4ea33ff1ec770c55
jjsparhawk <53868314+jjsparhawk@users.noreply.github.com>
2019-09-11 14:00:06 -0400

    Update SSCExample.js

--- SSCExample.js
+++ SSCExample.js
@@ -1188,7 +1188,7 @@ Hydra.post('printProperties', function(request, response) {
     var serverAuth = Hydra.Client.authServer();
     
     Logger.info(JSON.stringify(request));
-   return ({auth: serverAuth});
+   return (request);
     
 });
 
