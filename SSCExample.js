a80b8132485a274057e562b6398630308031ad4d
jjsparhawk <53868314+jjsparhawk@users.noreply.github.com>
2019-09-13 12:42:42 -0400

    Update SSCExample.js

--- SSCExample.js
+++ SSCExample.js
@@ -1184,6 +1184,9 @@ Hydra.get('custom_ping', function(request, response) {
 
 //Custom endpoint to print request properties
 Hydra.post('printProperties', function(request, response) {
+    
+   var serverAuth = Hydra.Client.authServer();
+    
    Logger.info(JSON.stringify(request));
    return (request, {auth: serverAuth, body: [["Access"]]})
 });
